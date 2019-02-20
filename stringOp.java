b5e37f4c1a7118b30d2a00dc2cbb157bd600df61
Nandha kumar S <mailstonandhakumar@gmail.com>
2019-02-20 14:23:17 +0530

    Update stringOp.java

--- stringOp.java
+++ stringOp.java
@@ -1,6 +1,6 @@
 import java.io.*;
 import java.util.*;
-   class stringOp{
+    class stringOp{
     public static void main(String args[]){
       Scanner sc=new Scanner(System.in);
       System.out.println("Enter The Input String:");
@@ -11,13 +11,13 @@ import java.util.*;
           for(int j=0;j<str.length();j++)
             {
               if(str.charAt(i)==str.charAt(j))
-              {
+            {
                 count++;
               }
             }
             if(count==1)
             {
-              System.out.print(str.charAt(i)+" ");
+              System.out.print(str.charAt(i));
               count=0;
             }
             else
